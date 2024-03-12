5e0f34969dac80cbfc475ba8631ff5421fd468db
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-12 03:30:34 -0700

    add conneting with contract and convert time function

--- client/utils/apiFeature.js
+++ client/utils/apiFeature.js
@@ -35,3 +35,21 @@ try {
 const fetchContract=(signerOrProvider)=>{
     new ethers.Contract(ChatAppABI,ChatappAddress,signerOrProvider);
 }
+
+export const connectingWithContract=async()=>{
+    try {
+        const web3modal =new Web3Modal();
+        const connection=await web3modal.connect();
+        const provider=new ethers.providers.Web3Provider(connection);
+        const signer=provider.getSigner();
+        const contract=fetchContract(signer);
+        return contract;
+    } catch (error) {
+        console.log(error)
+    }
+}
+ 
+export const convertTime=(time)=>{
+    const newTime=new Date(time.toNumber());
+    const realTime=newTime.getHours()+"/"+"/"+newTime.getMinutes()+"/"+newTime.getSeconds()+" Date:"+newTime.getDate()+"/"+(newTime.getMonth()+1)+"/"+newTime.getFullYear();
+}
