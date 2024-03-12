14867bb72a41bfeaa7bfbecd40189f8866acb0cf
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-12 06:45:22 -0700

    ..

---
+++ client/context/ChatAppContext.js
@@ -0,0 +1,14 @@
+"use client"
+import React, { useEffect,useState } from "react";
+import { CheckIfWalletConnected,connectingWithContract,connectwallet,convertTime } from "@/utils/apiFeature";
+
+export const ChatAppConnect=React.createContext();
+
+export const ChatAppProvider=({children})=>{
+    const title="Hi Welcome to blockchain Cat app";
+    return (
+        <ChatAppConnect.Provider>
+        {children}
+        </ChatAppConnect.Provider>
+    )
+}
--- client/utils/apiFeature.js
+++ client/utils/apiFeature.js
@@ -52,4 +52,5 @@ export const connectingWithContract=async()=>{
 export const convertTime=(time)=>{
     const newTime=new Date(time.toNumber());
     const realTime=newTime.getHours()+"/"+"/"+newTime.getMinutes()+"/"+newTime.getSeconds()+" Date:"+newTime.getDate()+"/"+(newTime.getMonth()+1)+"/"+newTime.getFullYear();
+    return realTime;
 }
