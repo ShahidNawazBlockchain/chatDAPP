3040e1155e7712580c648a16f48c5248ece5d615
Zahid Nawaz <151016187+ShahidNawazBlockchain@users.noreply.github.com>
2024-03-12 02:53:29 -0700

    check and connect wallet function are ready

--- client/utils/apiFeature.js
+++ client/utils/apiFeature.js
@@ -7,7 +7,7 @@ export const CheckIfWalletConnected=async()=>{
         if(!window.ethereum){
             return console.log("Install MetaMask");
         }
-        const accunts=await window.ethereum.request({
+        const accunts = await window.ethereum.request({
             method:"eth_account",
         })
         const firstAccount=accunts[0];
@@ -15,4 +15,19 @@ export const CheckIfWalletConnected=async()=>{
     } catch (error) {
         console.log(error)
     }
+}
+
+export const connectwallet=async()=>{
+try {
+    if(!window.ethereum){
+        return "please install meta mask"
+    }
+    const accounts = await window.ethereum.request({
+      method:"eth_requestAccounts"
+    });
+    const firstAccount=accounts[0];
+    return firstAccount;
+} catch (error) {
+    console.log(error)
+}
 }
